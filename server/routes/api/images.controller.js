d1fca5086215a32f49a04eca9cf783244f79d65f
Pedro Piury Blanquez <pedropiuryblanquez@MacBook-Air-de-Pedro.local>
2018-05-11 12:14:27 +0200

    Coordenadas de gps cogidas y convertidas

--- server/routes/api/images.controller.js
+++ server/routes/api/images.controller.js
@@ -17,21 +17,36 @@ router.post('/', upload.single('file'), function (req, res) {
     if (error)
       console.log('Error: ' + error.message);
     else {
-      console.log(exifData.gps.GPSLatitude)
+      // console.log(exifData.gps.GPSLatitude)
+      // console.log(parseInt(exifData.gps.GPSLatitude))
+
+      function ConvertDMSToDD(degrees, minutes, seconds, direction) {
+        var dd = degrees + minutes/60 + seconds/(60*60);
+    
+        if (direction == "S" || direction == "W") {
+            dd = dd * -1;
+        } // Don't do anything for N or E
+        return dd;
+    }
+      function ParseDMS(input) {
+        var parts = input.split(/[^\d\w]+/);
+        var lat = ConvertDMSToDD(exifData.gps.GPSLatitude[0], exifData.gps.GPSLatitude[1], exifData.gps.GPSLatitude[2], exifData.gps.GPSLatitudeRef);
+        var lng = ConvertDMSToDD(exifData.gps.GPSLongitude[0], exifData.gps.GPSLongitude[1], exifData.gps.GPSLongitude[2], exifData.gps.GPSLongitudeRef);
+      }
 
       let location = {
         type: 'Point',
-        coordinates: [exifData.gps.GPSLatitude,0]
+        coordinates: [ConvertDMSToDD(exifData.gps.GPSLatitude[0], exifData.gps.GPSLatitude[1], exifData.gps.GPSLatitude[2], exifData.gps.GPSLatitudeRef),ConvertDMSToDD(exifData.gps.GPSLongitude[0], exifData.gps.GPSLongitude[1], exifData.gps.GPSLongitude[2], exifData.gps.GPSLongitudeRef)]
       }
-      
+
       const image = new Pic({
         owner: req.user.id,
-        data: exifData ,
+        data: exifData,
         url: req.file.url,
         location: location
       });
-    
-      
+
+
       image.save().then(image => {
         cloudinary.uploader.upload(req.file.path, function (result) { console.log(result) })
         console.log(image)
@@ -43,9 +58,9 @@ router.post('/', upload.single('file'), function (req, res) {
     }
   });
 
-  
-  
-  
+
+
+
 });
 
 module.exports = router;
